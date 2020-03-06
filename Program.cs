2a0bf1821d62c2e5092ff5d17560425dfc195d3a
wWilsonWw <golubkin.timofey@yandex.ru>
2020-03-06 21:00:13 +0000

    Lol

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace gittest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello dotnet!!!!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
